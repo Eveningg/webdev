109f6c6044d0c062c96d3b3a70f36aad5418dcf0
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-29 01:11:25 +0000

    fixed UserController does not exist.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,9 +6,10 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function register(){
+    public function register()
+    {
 
         $data['title'] = 'Register';
         return view('user/register', $data);
-    }
+    }    
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,16 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>@yield('title' $title</title>
+    <title>@yield('title', $title</title>
 
 </head>
 
 <body>
-    
     <div class="container">
-        <h1>@yield('title',$title)</h1>
+        <h1>@yield('title', $title)</h1>
         @yield('content')
     </div>
+
 </body>
 
 </html>

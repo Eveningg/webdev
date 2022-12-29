b2a341de7bce5a64453c63cd3af15aabbc80665d
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-29 00:35:46 +0000

    imported bootstrap as my css

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('register', [UserController::class, 'register'])->name('register');

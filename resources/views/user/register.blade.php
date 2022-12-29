2489624e66a15e2627cdb75414d93b5922418d8c
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-29 22:40:36 +0000

    fixed registration page

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -2,22 +2,21 @@
 @section('content')
 <div class="row">
     <div class="col-md-6">
-        @if($errors->all())
+        @if($errors->any())
         @foreach($errors->all() as $err)
-        <p class="alert alert-danger">{{ $err}}</p>
+        <p class="alert alert-danger">{{ $err }}</p>
         @endforeach
         @endif
-        <form method="POST" action="{{ route('register.action') }}">
+        <form action="{{ route('register.action') }}" method="POST">
             @csrf
-
             <div class="mb-3">
                 <label>Name <span class="text-danger">*</span></label>
-                <input class="form-control" type = "text" name="name" value="{{ old('name') }}" />
+                <input class="form-control" type="text" name="name" value="{{ old('name') }}" />
             </div>
 
             <div class="mb-3">
                 <label>Username <span class="text-danger">*</span></label>
-                <input class="form-control" type = "text" name="username" value="{{ old('username') }}" />
+                <input class="form-control" type="username" name="username" value="{{ old('username') }}" />
             </div>
 
             <div class="mb-3">
@@ -27,13 +26,14 @@
 
             <div class="mb-3">
                 <label>Password Confirmation<span class="text-danger">*</span></label>
-                <input class="form-control" type="password" name="password_confirmation" />
+                <input class="form-control" type="password" name="password_confirm" />
             </div>
 
             <div class="mb-3">
                 <button class="btn btn-primary">Register</button>
                 <a class="btn btn-danger" href="{{ route('home') }}">Back</a>
             </div>
+            
         </form>
     </div>
 </div>

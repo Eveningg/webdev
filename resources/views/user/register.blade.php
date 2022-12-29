765938616e534edff1d1d6e44d4d5230685ec9eb
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-29 22:08:49 +0000

    created the registration page and route

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -1,4 +1,40 @@
 @extends('app')
 @section('content')
- 
+<div class="row">
+    <div class="col-md-6">
+        <form action="{{ route('register.action') }}">
+            @csrf
+
+            <div class="mb-3">
+                <label>Name <span class="text-danger">*</span></label>
+                <input class="form-control" name="name" value="{{ old('name') }}" />
+            </div>
+
+            <div class="mb-3">
+                <label>Username <span class="text-danger">*</span></label>
+                <input class="form-control" name="username" value="{{ old('username') }}" />
+            </div>
+
+            <div class="mb-3">
+                <label>Password <span class="text-danger">*</span></label>
+                <input class="form-control" name="password" />
+            </div>
+
+            <div class="mb-3">
+                <label>Password Confirmation<span class="text-danger">*</span></label>
+                <input class="form-control" name="password confirmation" />
+            </div>
+
+            <div class="mb-3">
+                <label>Password Confirmation<span class="text-danger">*</span></label>
+                <input class="form-control" name="password confirmation" />
+            </div>
+
+            <div class="mb-3">
+                <button class="btn btn-primary">Register</button>
+                <a class="btn btn-danger" href="{{ route('home') }}">Back</a>
+            </div>
+        </form>
+    </div>
+</div>
 @endsection

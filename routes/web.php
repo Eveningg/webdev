20ab7b655f9f009ca6ef41c9457f948aff49d168
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-29 22:08:51 +0000

    route created for the 'register' page

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 Route::get('register', [UserController::class, 'register'])->name('register');
+Route::post('register', [UserController::class, 'register_action'])->name('register.action');

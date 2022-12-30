e4d40aff5f396a1faa6d9e302141e273e5a8c6fe
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-30 01:43:00 +0000

    captail H

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home', ['title' => 'home']);
+    return view('home', ['title' => 'Home']);
 })->name('home');
 
 Route::get('register', [UserController::class, 'register'])->name('register');

de7decfd10f794f06adcaf5ce6815ba38b55b4d8
Eveningg <93998243+Eveningg@users.noreply.github.com>
2022-12-31 00:16:31 +0000

    centered auth user home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,9 +12,18 @@
 
 
 @auth
-<p>Welcome <b>{{ Auth::user()->name }}</b></p>
-<a class="btn btn-primary" href="{{ route('password') }}">Change Password</a>
-<a class="btn btn-danger" href="{{ route('logout') }}">Logout</a>
+<div class="text-center">
+    <p style="font-size:40px;">  <strong> Welcome <b>{{ Auth::user()->name }}</b> </strong></p>
+
+    <div class="btn-group-vertical">
+        <a class="btn btn-primary" href="{{ route('password') }}">Change Password</a>
+
+        <div class="margin">
+            <a class="btn btn-danger" href="{{ route('logout') }}">Logout</a>
+        </div>
+
+    </div>
+</div>
 @endauth
 @guest
 <div class="text-center">

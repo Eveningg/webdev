0eb077383c24625991724fcd36436600d524b505
Eveningg <93998243+Eveningg@users.noreply.github.com>
2023-01-01 15:18:01 +0000

    Fixed home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,39 +1,12 @@
 @extends('app')
 @section('content')
-
-<style>
-.btn {
-    min-width: 250px;
-}
-.margin {
-    margin-top: 10px;
-}
-</style>
-
-
 @auth
-<div class="text-center">
-    <p style="font-size:40px;">  <strong> Welcome <b>{{ Auth::user()->name }}</b> </strong></p>
-
-    <div class="btn-group-vertical">
-        <a class="btn btn-primary" href="{{ route('password') }}">Change Password</a>
-
-        <div class="margin">
-            <a class="btn btn-danger" href="{{ route('logout') }}">Logout</a>
-        </div>
-
-    </div>
-</div>
+<p>Welcome <b>{{ Auth::user()->name }}</b></p>
+<a class="btn btn-primary" href="{{ route('password') }}">Change Password</a>
+<a class="btn btn-danger" href="{{ route('logout') }}">Logout</a>
 @endauth
 @guest
-<div class="text-center">
-    <h1> <strong>Home Page</strong></h1>
-    <div class="btn-group-vertical">
-        <a class="btn btn-primary" href="{{ route('login') }}">Login</a>
-        <div class="margin">
-            <a class="btn btn-info" href="{{ route('register') }}">Register</a>
-        </div>
-    </div>
-</div>
+<a class="btn btn-primary" href="{{ route('login') }}">Login</a>
+<a class="btn btn-info" href="{{ route('register') }}">Register</a>
 @endguest
 @endsection
